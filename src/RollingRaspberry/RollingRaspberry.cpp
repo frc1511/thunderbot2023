5b16189a7ea9861eb04e67b82171b919cfecc8b4
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2023-02-13 20:39:02 -0500

    Finished RollingRaspberry networking, started working on validating robot poses

--- src/RollingRaspberry/RollingRaspberry.cpp
+++ src/RollingRaspberry/RollingRaspberry.cpp
@@ -11,7 +11,63 @@
 #include <fmt/core.h>
 #include <cstdint>
 
-#define VISION_MESSAGE_SIZE (sizeof(double) * 16)
+/*
+  Message structure:
+  
+  time_since_data - 4 bytes (float)
+  pose1_x         - 4 bytes (float)
+  pose1_y         - 4 bytes (float)
+  pose1_z         - 4 bytes (float)
+  pose1_qw        - 4 bytes (float)
+  pose1_qx        - 4 bytes (float)
+  pose1_qy        - 4 bytes (float)
+  pose1_qz        - 4 bytes (float)
+  pose1_error     - 4 bytes (float)
+  pose2_x         - 4 bytes (float)
+  pose2_y         - 4 bytes (float)
+  pose2_z         - 4 bytes (float)
+  pose2_qw        - 4 bytes (float)
+  pose2_qx        - 4 bytes (float)
+  pose2_qy        - 4 bytes (float)
+  pose2_qz        - 4 bytes (float)
+  pose2_error     - 4 bytes (float)
+*/
+#define VISION_MESSAGE_SIZE (sizeof(float) * 17)
+
+// Field dimensions.
+
+#define FIELD_X 16.54175_m
+#define FIELD_Y 8.0137_m
+
+// Areas of elevation.
+
+#define BLUE_CS_MIN_X 2.7_m
+#define BLUE_CS_MAX_X 4.91_m
+#define RED_CS_MIN_X (FIELD_X - BLUE_CS_MAX_X)
+#define RED_CS_MAX_X (FIELD_X - BLUE_CS_MIN_X)
+
+#define CS_MIN_Y 1.28_m
+#define CS_MAX_Y 4.07_m
+
+#define CS_ELEVATION 0.5_m
+
+// Tolerances
+
+#define VERTICAL_TOLERANCE 5_in
+#define HORIZONTAL_TOLERANCE 5_in
+
+bool RobotPoseEstimate::isAmbiguous() const {
+    double minError = std::min(error1, error2);
+    double maxError = std::max(error1, error2);
+
+    double ratio = -1;
+
+    if (maxError > 0.0) {
+        ratio = minError / maxError;
+    }
+
+    return ratio > 0.2 || ratio < 0;
+}
 
 RollingRaspberry::RollingRaspberry() {
     initServer();
@@ -119,10 +175,66 @@ void RollingRaspberry::process() {
 }
 
 std::map<units::second_t, frc::Pose2d> RollingRaspberry::getEstimatedRobotPoses() {
+    std::vector<RobotPoseEstimate> estimates;
+    {
+        std::lock_guard<std::mutex> lk(connectionMutex);
+
+        // Get the latest robot pose estimates and clear the list.
+        estimates = robotPoseEstimates;
+        robotPoseEstimates.clear();
+    }
+
+    std::map<units::second_t, frc::Pose2d> finalPoses;
+
+    for (const auto& estimate : estimates) {
+        // If no ambiguity, use the pose with the lower reprojection error.
+        if (!estimate.isAmbiguous()) {
+            frc::Pose3d goodPose = estimate.error1 < estimate.error2 ? estimate.pose1 : estimate.pose2;
+            finalPoses.emplace(estimate.timestamp, frc::Pose2d(goodPose.X(), goodPose.Y(), frc::Rotation2d(goodPose.Rotation().Z())));
+            continue;
+        }
+
+        /**
+         * Ambiguous D:
+         *
+         * Some ways to resolve the ambiguity include:
+         * - Check whether the robot pose is outside of the field boundaries.
+         * - Check whether the robot pose is above the field or below the field.
+         * - Compare the pose against the previous known pose and see if it's a reasonable change.
+         * - Check to see whether other estimated poses are similar.
+         */
+    }
+
+}
+
+bool RollingRaspberry::validatePose(frc::Pose3d pose) const {
+    units::meter_t x = pose.X(), y = pose.Y(), z = pose.Z();
+
+    // Check field boundaries.
+    const units::meter_t min_x = 0_m - HORIZONTAL_TOLERANCE,
+                         max_x = FIELD_X + HORIZONTAL_TOLERANCE;
+    const units::meter_t min_y = 0_m - HORIZONTAL_TOLERANCE,
+                         max_y = FIELD_Y + HORIZONTAL_TOLERANCE;
+
+    // Check if on Charge Station.
+    bool onChargeStation = false;
+    if (y >= CS_MIN_Y && y <= CS_MAX_Y) {
+        if ((x >= BLUE_CS_MIN_X && x <= BLUE_CS_MAX_X) ||
+            (x >= RED_CS_MIN_X  && x <= RED_CS_MAX_X)) {
+            onChargeStation = true;
+        }
+    }
+
+    // Check elevation.
+    const units::meter_t min_z = 0_m - VERTICAL_TOLERANCE,
+                         max_z = (onChargeStation ? CS_ELEVATION : 0_m) + VERTICAL_TOLERANCE;
+
+    return (x >= min_x && x <= max_x) &&
+           (y >= min_y && y <= max_y) &&
+           (z >= min_z && z <= max_z);
 }
 
 void RollingRaspberry::connectionThread(std::thread& self, int clientFileDesc) {
-
     char buf[VISION_MESSAGE_SIZE];
     while (true) {
         ssize_t bytesRead = read(clientFileDesc, buf, VISION_MESSAGE_SIZE);
@@ -142,36 +254,35 @@ void RollingRaspberry::connectionThread(std::thread& self, int clientFileDesc) {
         else if (bytesRead == VISION_MESSAGE_SIZE) {
             char* iter = buf;
 
-            auto get_double = [&iter]() -> double {
-                double value;
-                std::memcpy(&value, iter, sizeof(double));
-                iter += sizeof(double);
+            auto get_float = [&iter]() -> float {
+                float value;
+                std::memcpy(&value, iter, sizeof(float));
+                iter += sizeof(float);
                 return value;
             };
 
-            units::meter_t pose1TranslationX = units::meter_t(get_double());
-            units::meter_t pose1TranslationY = units::meter_t(get_double());
-            units::meter_t pose1TranslationZ = units::meter_t(get_double());
-            frc::Translation3d pose1Translation(pose1TranslationX, pose1TranslationY, pose1TranslationZ);
-            frc::Rotation3d pose1Rotation(frc::Quaternion(get_double(), get_double(), get_double(), get_double()));
-            double pose1Error = get_double();
-            units::meter_t pose2TranslationX = units::meter_t(get_double());
-            units::meter_t pose2TranslationY = units::meter_t(get_double());
-            units::meter_t pose2TranslationZ = units::meter_t(get_double());
-            frc::Translation3d pose2Translation(pose2TranslationX, pose2TranslationY, pose2TranslationZ);
-            frc::Rotation3d pose2Rotation(frc::Quaternion(get_double(), get_double(), get_double(), get_double()));
-            double pose2Error = get_double();
+            units::second_t timestamp(frc::Timer::GetFPGATimestamp() - units::second_t(get_float()));
+            units::meter_t pose1X(get_float());
+            units::meter_t pose1Y(get_float());
+            units::meter_t pose1Z(get_float());
+            frc::Translation3d pose1Translation(pose1X, pose1Y, pose1Z);
+            frc::Rotation3d pose1Rotation(frc::Quaternion(get_float(), get_float(), get_float(), get_float()));
+            double pose1Error = get_float();
+            units::meter_t pose2X(get_float());
+            units::meter_t pose2Y(get_float());
+            units::meter_t pose2Z(get_float());
+            frc::Translation3d pose2Translation(pose2X, pose2Y, pose2Z);
+            frc::Rotation3d pose2Rotation(frc::Quaternion(get_float(), get_float(), get_float(), get_float()));
+            double pose2Error = get_float();
+
+            RobotPoseEstimate estimate(timestamp, frc::Pose3d(pose1Translation, pose1Rotation), frc::Pose3d(pose2Translation, pose2Rotation), pose1Error, pose2Error);
 
             {
-                std::scoped_lock lock(connectionMutex);
-                robotPoseEstimates.emplace_back(
-                    frc::Pose3d(pose1Translation, pose1Rotation), frc::Pose3d(pose2Translation, pose2Rotation),
-                    pose1Error, pose2Error
-                );
+                std::lock_guard<std::mutex> lk(connectionMutex);
+                robotPoseEstimates.push_back(std::move(estimate));
             }
         }
     }
-
 
     self.detach();
 }
--- src/RollingRaspberry/RollingRaspberry.h
+++ src/RollingRaspberry/RollingRaspberry.h
@@ -4,10 +4,10 @@
 
 #include <frc/smartdashboard/Smartdashboard.h>
 #include <frc/geometry/Pose2d.h>
-#include <frc/Timer.h>
+#include <units/time.h>
 #include <units/length.h>
 #include <units/angle.h>
-#include <Util/BetterThread.h>
+#include <thread>
 #include <mutex>
 #include <map>
 #include <cstddef>
@@ -17,16 +17,30 @@
 #include <frc/geometry/Transform3d.h>
 #include <frc/geometry/Pose3d.h>
 
+/**
+ * Represents an estimate of the robot's pose based on vision data.
+ */
 struct RobotPoseEstimate {
+    units::second_t timestamp;
+
     frc::Pose3d pose1;
     frc::Pose3d pose2;
 
     double error1;
     double error2;
 
-    double GetAmbiguity() const;
+    /**
+     * Returns whether the estimate is likely to be ambiguous.
+     * 
+     * Calculates the ratio of pose reprojection errors and returns true
+     * if the ratio is above 0.2.
+    */
+    bool isAmbiguous() const;
 };
 
+/**
+ * Mechanism that handles communication with the Raspberry Pis on the robot.
+ */
 class RollingRaspberry : public Mechanism {
 public:
     RollingRaspberry();
@@ -35,12 +49,18 @@ public:
     void process() override;
 
     /**
-     * Returns the estimated poses of the robot calculated by the Raspberry
-     * Pi's vision processing pipeline paired with a FPGA Timestamp.
+     * Returns the most recent estimated robot poses.
      */
     std::map<units::second_t, frc::Pose2d> getEstimatedRobotPoses();
 
 private:
+    /**
+     * Validates a pose by checking several things:
+     *   - Whether the pose is within the field boundaries.
+     *   - Whether the pose is above the field or below the field.
+     */
+    bool validatePose(frc::Pose3d pose) const;
+
     bool serverRunning = false;
     void initServer();
 
@@ -53,7 +73,7 @@ private:
     std::mutex connectionMutex;
 
     std::map<int, std::thread> connectionThreads;
-    void connectionThread(std::thread& self, int client_fd);
+    void connectionThread(std::thread& self, int clientFileDesc);
 
     std::vector<RobotPoseEstimate> robotPoseEstimates;
 };
